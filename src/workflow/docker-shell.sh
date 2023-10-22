6c95dae6aad57d55834170e4723a9cc7693d664a
ai-xiaihai <wknb1996@gmail.com>
2023-10-22 15:45:27 -0400

    permission issue solved hopefully

--- src/workflow/docker-shell.sh
+++ src/workflow/docker-shell.sh
@@ -7,7 +7,7 @@ export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../../secrets/
 export GCP_PROJECT="ac215-radiq"
 export GCS_BUCKET_NAME="x-ray-app-ml-workflow-demo"
-export GCS_SERVICE_ACCOUNT="radiq-app-data@ac215-radiq.iam.gserviceaccount.com"
+export GCS_SERVICE_ACCOUNT="ml-workflow@ac215-radiq.iam.gserviceaccount.com"
 export GCP_REGION="us-central1"
 export GCS_PACKAGE_URI="gs://x-ray-app-trainer-code"
 
