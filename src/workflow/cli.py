dbcb78f092ac09523ff25247e70844cd5fa29c44
Linglai Chen <linglai27@yahoo.com>
2023-10-26 12:19:51 -0400

    enable initiating serverless training

--- src/workflow/cli.py
+++ src/workflow/cli.py
@@ -9,10 +9,11 @@ import os
 import argparse
 import random
 import string
+import subprocess
 from kfp import dsl
 from kfp import compiler
 import google.cloud.aiplatform as aip
-# from model import model, dataset_mscxr
+from model_workflow import model_training
 
 
 GCP_PROJECT = os.environ["GCP_PROJECT"]
@@ -24,7 +25,7 @@ GCS_PACKAGE_URI = os.environ["GCS_PACKAGE_URI"]
 GCP_REGION = os.environ["GCP_REGION"]
 
 # DATA_COLLECTOR_IMAGE = "gcr.io/ac215-project/mushroom-app-data-collector"
-DATA_PREPROCESSOR_IMAGE = "dlops/x-ray-app-data-preprocessor"
+DATA_PREPROCESSOR_IMAGE = "dlops/mushroom-app-data-processor"
 DATA_SPLITTER_IMAGE = "dlops/x-ray-app-data-splitting"
 
 
@@ -35,126 +36,31 @@ def generate_uuid(length: int = 8) -> str:
 def main(args=None):
     print("CLI Arguments:", args)
 
-    if args.data_preprocessor:
-        print("Data Preprocessor")
+    if args.model_workflow:
+        print("Serverless Training Pipeline")
 
-        # Define a Container Component for data processor
-        @dsl.container_component
-        def data_preprocessor():
-            container_spec = dsl.ContainerSpec(
-                image=DATA_PREPROCESSOR_IMAGE,
-                command=[],
-                args=[
-                    "cli.py",
-                    "--clean",
-                    "--prepare",
-                    f"--bucket {GCS_BUCKET_NAME}",
-                ],
-            )
-            return container_spec
-
-        # Define a Pipeline
         @dsl.pipeline
-        def data_preprocessor_pipeline():
-            data_preprocessor()
+        def model_training_pipeline():
+            model_training(
+                project=GCP_PROJECT,
+                location=GCP_REGION,
+                staging_bucket=GCS_PACKAGE_URI,
+                bucket_name=GCS_BUCKET_NAME,
+            )
 
         # Build yaml file for pipeline
         compiler.Compiler().compile(
-            data_preprocessor_pipeline, package_path="data_preprocessor.yaml"
+            model_training_pipeline, package_path="model_training.yaml"
         )
 
         # Submit job to Vertex AI
         aip.init(project=GCP_PROJECT, staging_bucket=BUCKET_URI)
 
         job_id = generate_uuid()
-        DISPLAY_NAME = "mushroom-app-data-preprocessor-" + job_id
+        DISPLAY_NAME = "x-ray-app-model-training-" + job_id
         job = aip.PipelineJob(
             display_name=DISPLAY_NAME,
-            template_path="data_preprocessor.yaml",
-            pipeline_root=PIPELINE_ROOT,
-            enable_caching=False,
-        )
-
-        job.run(service_account=GCS_SERVICE_ACCOUNT)
-
-    if args.pipeline:
-        # Define a Container Component for data collector
-        @dsl.container_component
-        def data_collector():
-            container_spec = dsl.ContainerSpec(
-                image=DATA_COLLECTOR_IMAGE,
-                command=[],
-                args=[
-                    "cli.py",
-                    "--search",
-                    "--nums 50",
-                    "--query oyster+mushrooms crimini+mushrooms amanita+mushrooms",
-                    f"--bucket {GCS_BUCKET_NAME}",
-                ],
-            )
-            return container_spec
-
-        # Define a Container Component for data processor
-        @dsl.container_component
-        def data_processor():
-            container_spec = dsl.ContainerSpec(
-                image=DATA_PROCESSOR_IMAGE,
-                command=[],
-                args=[
-                    "cli.py",
-                    "--clean",
-                    "--prepare",
-                    f"--bucket {GCS_BUCKET_NAME}",
-                ],
-            )
-            return container_spec
-
-        # Define a Pipeline
-        @dsl.pipeline
-        def ml_pipeline():
-            # Data Collector
-            data_collector_task = data_collector().set_display_name("Data Collector")
-            # Data Processor
-            data_processor_task = (
-                data_processor()
-                .set_display_name("Data Processor")
-                .after(data_collector_task)
-            )
-            # Model Training
-            model_training_task = (
-                model_training(
-                    project=GCP_PROJECT,
-                    location=GCP_REGION,
-                    staging_bucket=GCS_PACKAGE_URI,
-                    bucket_name=GCS_BUCKET_NAME,
-                    epochs=15,
-                    batch_size=16,
-                    model_name="mobilenetv2",
-                    train_base=False,
-                )
-                .set_display_name("Model Training")
-                .after(data_processor_task)
-            )
-            # Model Deployment
-            model_deploy_task = (
-                model_deploy(
-                    bucket_name=GCS_BUCKET_NAME,
-                )
-                .set_display_name("Model Deploy")
-                .after(model_training_task)
-            )
-
-        # Build yaml file for pipeline
-        compiler.Compiler().compile(ml_pipeline, package_path="pipeline.yaml")
-
-        # Submit job to Vertex AI
-        aip.init(project=GCP_PROJECT, staging_bucket=BUCKET_URI)
-
-        job_id = generate_uuid()
-        DISPLAY_NAME = "mushroom-app-pipeline-" + job_id
-        job = aip.PipelineJob(
-            display_name=DISPLAY_NAME,
-            template_path="pipeline.yaml",
+            template_path="model_training.yaml",
             pipeline_root=PIPELINE_ROOT,
             enable_caching=False,
         )
@@ -241,6 +147,12 @@ if __name__ == "__main__":
         "--pipeline",
         action="store_true",
         help="Mushroom App Pipeline",
+    )
+    parser.add_argument(
+        "-m",
+        "--model_workflow",
+        action="store_true",
+        help="Serverless training",
     )
     parser.add_argument(
         "-s1",
